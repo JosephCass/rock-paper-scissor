3adfa022f10422f31e3f653e8143c344e75a5c3f
JosephCass <josephcassinera@gmail.com>
2022-07-13 11:03:10 -0400

    Added UI To Rock Paper Scissor Project

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
+let startButton = document.querySelector(".game-start");
+let mainContainer = document.querySelector(".main-container");
+let gameContainer = document.querySelector(".game-container");
+let playerButtons = document.querySelectorAll(".player-button");
+let playerButtonsContainer = document.querySelector(
+  ".player-buttons-container"
+);
+let computerButtons = document.querySelectorAll(".computer-button");
+let playerScoreSpan = document.querySelector(".player-score");
+let computerScoreSpan = document.querySelector(".computer-score");
+let roundWinner = document.querySelector(".round-winner");
+let roundWinnerText = document.querySelector(".round-winner-text");
+let playerWinsDisplay = document.querySelector(".player-wins-display");
+let playerWinsText = document.querySelector("player-wins-text");
+let computerWinsDisplay = document.querySelector(".computer-wins-display");
+let computerWinsText = document.querySelector("computer-wins-text");
+
+let playerSelection;
+let playerScore = 0;
+let computerScore = 0;
+let rounds = 5;
+
+//Function to Generate Computers Choice
 function computerPlay() {
   let randomInt = Math.floor(Math.random() * 3) + 1;
   if (randomInt === 1) {
@@ -11,68 +34,134 @@ function computerPlay() {
   }
 }
 
-function getPlayerChoice() {
-  return prompt("Choose Rock, Paper or Scissor").toLocaleLowerCase();
+//Get Players Choice
+function getPlayerChoice(e) {
+  if (e.target.classList.contains("rock")) {
+    playerSelection = "rock";
+  }
+  if (e.target.classList.contains("paper")) {
+    playerSelection = "paper";
+  }
+  if (e.target.classList.contains("scissor")) {
+    playerSelection = "scissor";
+  }
 }
 
-const playerSelection = getPlayerChoice();
-const computerSelection = computerPlay();
-
+//Function for playing a round & deciding who won the round
 function playRound() {
-  const playerChoice = getPlayerChoice();
-  const computerChoice = computerPlay();
+  let playerChoice = playerSelection;
+  let computerChoice = computerPlay();
 
   if (playerChoice === "rock") {
     if (computerChoice === "paper") {
       console.log("Computer Wins Paper beats Rock");
-      return 2;
+      return [2, computerChoice];
     } else if (computerChoice === "scissor") {
       console.log("You Win Rock beats Scissors");
-      return 1;
+      return [1, computerChoice];
+    } else if (computerChoice === "rock") {
+      console.log("You Tied Rock & Rock");
+      return [3, computerChoice];
     }
   }
   if (playerChoice === "paper") {
     if (computerChoice === "scissor") {
       console.log("Computer Wins Scissors beats Paper");
-      return 2;
+      return [2, computerChoice];
     } else if (computerChoice === "rock") {
       console.log("You Win Paper beats Rock");
-      return 1;
+      return [1, computerChoice];
+    } else if (computerChoice === "paper") {
+      console.log("You Tied Paper & Paper");
+      return [3, computerChoice];
     }
   }
   if (playerChoice === "scissor") {
     if (computerChoice === "rock") {
       console.log("Computer Wins Rock beats Scissors");
-      return 2;
+      return [2, computerChoice];
     } else if (computerChoice === "paper") {
       console.log("You Win Scissors beats Paper");
-      return 1;
+      return [1, computerChoice];
+    } else if (computerChoice === "scissor") {
+      console.log("You Tied Scissor & Scissor");
+      return [3, computerChoice];
     }
   }
 }
 
-function checkWinner(player, computer) {
-  if (player > computer) {
-    console.log(`Player Beats Computer ${player} to ${computer}`);
-  } else if (computer > player) {
-    console.log(`Computer Beats Player ${computer} to ${player}`);
-  } else if (player === computer) {
-    console.log(`Draw Player:${player} Computer:${computer}`);
+function displayGameWinner(winz) {
+  winz.classList.remove("hidden");
+  roundWinner.classList.add("hidden");
+  playerButtonsContainer.classList.add("hidden");
+}
+
+//Update UI Based On Winner for Round and Game To 5
+function updateUI(e) {
+  let results = playRound();
+  console.log(results);
+  if (results[0] === 1) {
+    playerButtons.forEach(function (e) {
+      e.style.backgroundColor = "white";
+    });
+    e.target.style.backgroundColor = "green";
+    playerScore++;
+    playerScoreSpan.textContent = playerScore;
+    roundWinnerText.textContent = `👤 You Win The Round ${playerSelection} beats ${results[1]}`;
+    computerButtons.forEach(function (e) {
+      e.style.backgroundColor = "white";
+      if (e.classList.contains(results[1])) {
+        e.style.backgroundColor = "red";
+      }
+    });
+  }
+  if (results[0] === 2) {
+    playerButtons.forEach(function (e) {
+      e.style.backgroundColor = "white";
+    });
+    e.target.style.backgroundColor = "red";
+    computerScore++;
+    computerScoreSpan.textContent = computerScore;
+    roundWinnerText.textContent = `🤖 Computer Wins The Round ${results[1]} beats ${playerSelection}`;
+    computerButtons.forEach(function (e) {
+      e.style.backgroundColor = "white";
+      if (e.classList.contains(results[1])) {
+        e.style.backgroundColor = "green";
+      }
+    });
+  }
+  if (results[0] === 3) {
+    playerButtons.forEach(function (e) {
+      e.style.backgroundColor = "white";
+    });
+    e.target.style.backgroundColor = "yellow";
+    roundWinnerText.textContent = `Draw ${playerSelection} and ${results[1]} Tie`;
+    computerButtons.forEach(function (e) {
+      e.style.backgroundColor = "white";
+      if (e.classList.contains(results[1])) {
+        e.style.backgroundColor = "yellow";
+      }
+    });
+  }
+  if (playerScore === 5) {
+    displayGameWinner(playerWinsDisplay);
+  } else if (computerScore === 5) {
+    displayGameWinner(computerWinsDisplay);
   }
 }
 
-function game() {
-  let playerScore = 0;
-  let computerScore = 0;
-  for (let i = 0; i < 6; i++) {
-    let winner = setTimeout(playRound(), 3000);
-    if (winner === 1) {
-      playerScore++;
-    } else if (winner === 2) {
-      computerScore++;
-    }
-  }
-  checkWinner(playerScore, computerScore);
-}
+//Start Game Functionality
+startButton.addEventListener("click", function () {
+  mainContainer.classList.remove("contain");
+  gameContainer.classList.remove("hidden");
+  startButton.classList.add("hidden");
+});
 
-game();
+//Button Functionality
+document.addEventListener("click", function (e) {
+  console.log(e.target.classList);
+  if (e.target.classList.contains("player-button")) {
+    getPlayerChoice(e);
+    updateUI(e);
+  }
+});
